c03047cc4aded46d6d2648223dbe015e02e14516
Alex Hoffman <alexhoffman617@gmail.com>
2012-10-24 22:05:36 -0400

    HW5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-fPlusdollar2::Application.configure do
+Plusdollar2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
